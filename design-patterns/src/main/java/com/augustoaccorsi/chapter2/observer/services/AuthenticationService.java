764a68cfbaa3a6227a067b83e7e23e8fa879c2b9
augustoaccorsi <augusto.acorsi@gmail.com>
2021-11-21 14:20:16 -0300

    add priority

--- design-patterns/src/main/java/com/augustoaccorsi/chapter2/observer/services/AuthenticationService.java
+++ design-patterns/src/main/java/com/augustoaccorsi/chapter2/observer/services/AuthenticationService.java
@@ -2,10 +2,11 @@ package com.augustoaccorsi.chapter2.observer.services;
 
 import com.augustoaccorsi.chapter2.observer.Customer;
 
+import javax.annotation.Priority;
 import javax.enterprise.event.Observes;
 
 public class AuthenticationService {
-    public void createAuthenticationCredentials(@Observes Customer customer){
+    public void createAuthenticationCredentials(@Observes @Priority(100) Customer customer){
         // write the authentication
         System.out.println("Authentication");
     }
--- design-patterns/src/main/java/com/augustoaccorsi/chapter2/observer/services/CustomerService.java
+++ design-patterns/src/main/java/com/augustoaccorsi/chapter2/observer/services/CustomerService.java
@@ -2,10 +2,11 @@ package com.augustoaccorsi.chapter2.observer.services;
 
 import com.augustoaccorsi.chapter2.observer.Customer;
 
+import javax.annotation.Priority;
 import javax.enterprise.event.Observes;
 
 public class CustomerService {
-    public void createCustomer(@Observes Customer customer){
+    public void createCustomer(@Observes @Priority(10) Customer customer){
         // write code
         System.out.println("Customer");
     }
